31aa8ba8c628a688cf6cb57779ff73b5944ac3b3
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-06-20 13:27:19 +0000

    Update Tokens

--- token-farm/dist/js/global.js
+++ token-farm/dist/js/global.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 20 Jun 2022 13:22:32 GMT
+ * Generated on Mon, 20 Jun 2022 13:27:18 GMT
  */
 
 export const magenta100 = "#fde8f2";
--- token-farm/dist/js/mw-theme_dark.js
+++ token-farm/dist/js/mw-theme_dark.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 20 Jun 2022 13:22:32 GMT
+ * Generated on Mon, 20 Jun 2022 13:27:18 GMT
  */
 
 export const fgDefault = "#d3126c";
