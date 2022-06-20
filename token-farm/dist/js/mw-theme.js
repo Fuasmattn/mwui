970fb4e8af02083e15f72cbf69677d2660be57b5
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-06-20 19:28:56 +0000

    Update Tokens

--- token-farm/dist/js/mw-theme.js
+++ token-farm/dist/js/mw-theme.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 20 Jun 2022 19:26:21 GMT
+ * Generated on Mon, 20 Jun 2022 19:28:46 GMT
  */
 
 export const semanticColorActionsFilledDefaultBgColor = "#ed2c86";
