dc6c52b232f17af54ff199cabcb967ab826eaa75
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-06-21 09:57:54 +0000

    Update Tokens

--- library/dist/index.mjs
+++ library/dist/index.mjs
@@ -1370,7 +1370,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 /**
  * Do not edit directly
- * Generated on Tue, 21 Jun 2022 09:50:17 GMT
+ * Generated on Tue, 21 Jun 2022 09:56:56 GMT
  */
 const overline = {
   "fontFamily": "Roboto",
