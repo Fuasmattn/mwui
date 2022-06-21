ea698bfe1675ece90f8a2409bd7c3db4b09f5453
Martin Prinz <martin.prinz@maibornwolff.de>
2022-06-21 13:28:17 +0000

    fix entry point

--- token-farm/dist/js/mw-theme.js
+++ token-farm/dist/js/mw-theme.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Tue, 21 Jun 2022 13:27:04 GMT
+ * Generated on Tue, 21 Jun 2022 13:27:40 GMT
  */
 
 export const semanticColorActionsFilledDefaultBgColor = "#ed2c86";
