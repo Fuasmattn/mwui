021ea7034c907d482a9ac15c353e7c02a36fd687
Martin Prinz <martin.prinz@maibornwolff.de>
2022-06-21 10:10:06 +0000

    improve type generation

--- library/dist/index.mjs
+++ library/dist/index.mjs
@@ -1370,7 +1370,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 /**
  * Do not edit directly
- * Generated on Tue, 21 Jun 2022 10:08:10 GMT
+ * Generated on Tue, 21 Jun 2022 10:09:19 GMT
  */
 const overline = {
   "fontFamily": "Roboto",
--- token-farm/dist/js/mw-theme_dark.js
+++ token-farm/dist/js/mw-theme_dark.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Tue, 21 Jun 2022 10:08:10 GMT
+ * Generated on Tue, 21 Jun 2022 10:09:19 GMT
  */
 
 export const fgDefault = "#d3126c";
