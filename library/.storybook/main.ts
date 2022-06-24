02cbba802000c494054e2b1d272dceeb69d53e0c
Martin Prinz <martin.prinz@maibornwolff.de>
2022-06-24 13:44:00 +0000

    fix sb mdx setup

--- library/.storybook/main.ts
+++ library/.storybook/main.ts
@@ -1,30 +1,17 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
   /** Expose public folder to storybook as static */
   staticDirs: ["../public"],
   features: {
-    previewMdx2: true,
+    // previewMdx2: true,
   },
   addons: [
+    "@storybook/addon-docs",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    '@storybook/addon-controls',
-    "@storybook/preset-scss",
-    "@storybook/addon-docs",
-    {
-      /**
-       * Fix Storybook issue with PostCSS@8
-       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
-       */
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
+    "@storybook/addon-controls",
   ],
   core: {
     builder: "webpack5",
--- library/.storybook/preview.ts
+++ library/.storybook/preview.ts
@@ -1,3 +1,13 @@
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { themes } from "@storybook/theming";
+
+import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
+
+SyntaxHighlighter.registerLanguage("javascript", javascript);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  docs: {
+    theme: themes.normal,
+  },
 };
