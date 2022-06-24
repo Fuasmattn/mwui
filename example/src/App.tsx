f9c9db388776e694d44faae95143609accd8e00b
Martin Prinz <martin.prinz@maibornwolff.de>
2022-06-24 09:03:47 +0000

    add card to example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { Button } from "@fuasmattn/mwui";
+import { Card } from "@fuasmattn/mwui";
 
 function App() {
   return (
-    <div>
-      <Button label="Hello World" onClick={() => {}}></Button>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, 360px)",
+        gridGap: "32px",
+      }}
+    >
+      <Card
+        title="Hello"
+        body="this is dog."
+        imageAlt="a dog"
+        imageSrc="https://placedog.net/720/338?r"
+        confirmLabel="Confirm"
+        onConfirm={() => {}}
+      />
     </div>
   );
 }
